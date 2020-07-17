03dddd3247e8bcf3cb61e1cb1bfd222c0a413f14
Chad <chad.lines1@gmail.com>
2020-07-17 13:43:48 -0600

    URLs, Views, and Templates

--- djangog_blog/blog/urls.py
+++ djangog_blog/blog/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from . import views             # '.' indicates the current directory
 
 urlpatterns = [
+    # path('', views.post_list, name='post_list')
     path(
         '',                 # This is the root URL (127.0.0.1:8000)
         views.post_list,    # This is the post_list view in the views.py file
