aa4cc3ed1e3b73046fa251334d3fe52b9c4b5f05
Chad <chad.lines1@gmail.com>
2020-07-29 12:06:09 -0600

    Forms 2
    Editing

--- djangog_blog/blog/urls.py
+++ djangog_blog/blog/urls.py
@@ -10,4 +10,5 @@ urlpatterns = [
     ),
     path('post/<int:pk>/', views.post_detail, name='post_detail'),  # Path for a dedicated post view
     path('post_new', views.post_new, name='post_new'),
+    path('post/<int:pk>/edit/', views.post_edit, name='post_edit'),
 ]
